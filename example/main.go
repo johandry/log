129ba96020b013010eb4277cf46cab10390a1975
Johandry Amador <johandry@yahoo.com>
2017-01-08 02:08:15 -0800

    Example with new formatter

--- example/main.go
+++ example/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/johandry/log"
 	"github.com/spf13/viper"
 )
@@ -11,12 +12,12 @@ const configfilename = "config"
 
 func initOne() {
 	// Set default for the log filename
-	viper.SetDefault("log_file", "default.log")
-	viper.SetDefault("log_level", "info")
+	viper.SetDefault(log.FilenameKey, "default.log")
+	viper.SetDefault(log.LevelKey, "info")
 
 	// Bind the 'log' parameter to the LOG environment variable
-	viper.BindEnv("log_file")
-	viper.BindEnv("log_level")
+	viper.BindEnv(log.FilenameKey)
+	viper.BindEnv(log.LevelKey)
 
 	//Allow the option to set the values in a config file
 	viper.SetConfigName(configfilename)
@@ -25,24 +26,25 @@ func initOne() {
 }
 
 func initTwo() {
-	viper.Set("log_file", "set.log")
-	viper.Set("log_level", "error")
+	viper.Set(log.FilenameKey, "set.log")
+	viper.Set(log.LevelKey, "debug")
 }
 
 func main() {
 	// You may want to do this at the console. However, the parameter in the
 	// config file has priority
-	if os.Getenv("log_file") != "" {
-		os.Setenv("log_file", "environment.log")
+	if os.Getenv(log.FilenameKey) != "" {
+		os.Setenv(log.FilenameKey, "environment.log")
 	}
-	if os.Getenv("log_level") != "" {
-		os.Setenv("log_level", "debug")
+	if os.Getenv(log.LevelKey) != "" {
+		os.Setenv(log.LevelKey, "debug")
 	}
 
 	// initOne()
 	initTwo()
+	viper.Set(log.OutputKey, os.Stderr)
 
-	log.Prefix("main").Info("Information")
+	log.Prefix("main").WithFields(logrus.Fields{"key": "value", "env": "test testea"}).Info("Information")
 	log.Std().Debug("Debuging")
 	log.Prefix("main").Warn("Warning")
 	log.Prefix("main").Error("Error")
